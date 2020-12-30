064be78f3c2db421411a0ef36ce21dafb05d5458
sibishiva <sibishiva@live.com>
2020-12-30 01:27:39 -0800

    Login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Dashboard from '../src/components/Dashboard/Dashboard';
 import Preferences from '../src/components/Preferences/Preferences';
+import Login from '../src/components/Login/Login';
 
 function App() {
+	const [token, setToken] = useState();
+
+	if (!token) {
+		return <Login setToken={setToken} />;
+	}
+
 	return (
 		<div className="wrapper">
 			<h1>Application</h1>
